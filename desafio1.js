44ac901f512e96e86f2f982a83805c5616dfa259
Mauricio Fioretti <mauriciofioretti@gmail.com>
2023-12-21 20:11:26 -0300

    Mejoras de pruebas

--- desafio1.js
+++ desafio1.js
@@ -3,11 +3,11 @@ class ProductManager {
         this.products = []
     }
 
-    getProduct(){
+    getProducts(){
         console.log(this.products)
     }
 
-    addProduct(title, description, price, thumbnail, code, stock){
+    addProducts(title, description, price, thumbnail, code, stock){
         if(!title || !description || !price || !thumbnail || !code || !stock){
             console.log(`Todos los datos son obligatorios!`)
         }
@@ -40,19 +40,23 @@ class ProductManager {
 
 const productos = new ProductManager()
 
+//Preguntar por todos los productos agregados
+console.log(`\nPrueba de todos los productos agregados`)
+productos.getProducts()
+
 //Agregar productos a la class
 console.log(`\nAgregando los libros`)
-productos.addProduct('OMA 1', 'Problemas de olimpiadas nivel 1', '$4000', 'OMA1.jpg', 1234, 3)
-productos.addProduct('OMA 2', 'Problemas de olimpiadas nivel 2', '$4000', 'OMA2.jpg', 1235, 3)
-productos.addProduct('OMA 3', 'Problemas de olimpiadas nivel 3', '$4000', 'OMA3.jpg', 1236, 3)
+productos.addProducts('OMA 1', 'Problemas de olimpiadas nivel 1', '$4000', 'OMA1.jpg', 1234, 3)
+productos.addProducts('OMA 2', 'Problemas de olimpiadas nivel 2', '$4000', 'OMA2.jpg', 1235, 3)
+productos.addProducts('OMA 3', 'Problemas de olimpiadas nivel 3', '$4000', 'OMA3.jpg', 1236, 3)
 
 //Validar todos los campos son oblicatorios
 console.log(`\nPrueba de todos los campos son obligatorios`)
-productos.addProduct('OMA 3', 'Problemas de olimpiadas nivel 3', 'OMA3.jpg', 1236, 3)
+productos.addProducts('OMA 3', 'Problemas de olimpiadas nivel 3', 'OMA3.jpg', 1236, 3)
 
 //Preguntar por todos los productos agregados
 console.log(`\nPrueba de todos los productos agregados`)
-productos.getProduct()
+productos.getProducts()
 
 //Preguntar por un producto segun su id
 console.log(`\nPrueba de buscar un producto segun su id`)
